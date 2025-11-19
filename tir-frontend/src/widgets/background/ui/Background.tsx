badda94519ffcbf79a841fc661b054293c3b49c8
Ruslan Skv <skvirya@mail.ru>
2025-11-19 14:23:50 +0300

    feat: добавил фильтр  для светлой темы

--- tir-frontend/src/widgets/background/ui/Background.tsx
+++ tir-frontend/src/widgets/background/ui/Background.tsx
@@ -5,10 +5,18 @@ import type { RootState } from '@/shared/lib/redux';
 
 export const Background: React.FC = () => {
   const isDarkTheme = useSelector((state: RootState) => state.theme.isDarkTheme);
+  // useSelector((state: RootState) => state.theme.isDarkTheme);
 
   // Для избежания mismatch, используем только светлую тему на сервере
   // На клиенте тема определится корректно после гидрации
-  const backgroundImage = isDarkTheme ? "url('/images/dark-fon.jpg')" : "url('/images/fon.jpg')";
+  // const backgroundImage = isDarkTheme ? "url('/images/dark-fon.jpg')" : "url('/images/fon.jpg')";
+  const backgroundImage = "url('/images/dark-fon.jpg')";
 
-  return <div className={styles.background} style={{ backgroundImage }} />;
+  return (
+    <div
+      className={styles.background}
+      style={{ backgroundImage }}
+      data-theme={isDarkTheme ? 'dark' : 'light'}
+    />
+  );
 };
